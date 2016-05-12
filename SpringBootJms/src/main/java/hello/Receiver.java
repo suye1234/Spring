964b782cfb00d28462d169fa2222653ceaba4b50
suye1234 <suye0803@139.com>
2016-05-12 19:41:43 +0800

    Add spring boot jms
    Add spring boot jms

---
+++ SpringBootJms/src/main/java/hello/Receiver.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package hello;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileSystemUtils;
+
+/**
+ * @author ye.su
+ * Email: suye@ehousechina.com
+ * May 10, 2016
+ *
+ */
+@Component
+public class Receiver {
+	 /**
+     * Get a copy of the application context
+     */
+    @Autowired
+    ConfigurableApplicationContext context;
+
+    /**
+     * When you receive a message, print it out, then shut down the application.
+     * Finally, clean up any ActiveMQ server stuff.
+     */
+    @JmsListener(destination = "mailbox-destination", containerFactory = "myJmsContainerFactory")
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        context.close();
+        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+    }
+
+	
+}
